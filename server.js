7c04c1231ec0f3090f96aebd2d82e3f5ec1b8df4
YUSUF IDRIS <yusuffidris97@gmail.com>
2022-07-06 18:15:50 +0100

    idris

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const favourite = process.env.NAME;
+const fan = process.env.NAME;
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 async function main() {
   while(true) {
-    console.log (`${favourite} rocks!`);
+    console.log (`${fan} rocks!`);
     await sleep(5000);
     await sleep(5000);
   }
